e75cdb41b493699caf25a5bd0e51a39cd5feb8fb
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2015-11-17 16:10:05 +0000

    Made FedoraFileService return files as streams.

--- app/modules/hilda/services/fedora_file_service.rb
+++ app/modules/hilda/services/fedora_file_service.rb
@@ -2,6 +2,63 @@ module Hilda
   module Services
     class FedoraFileService
 
+      class FedoraStreamIO
+        def initialize(fedora_file)
+          @fedora_file = fedora_file
+          @closed = false
+          rewind
+        end
+
+        def read(amount=nil, buf=nil)
+          raise IOError, "stream closed" if @closed
+          buf ||= ''
+          buf.clear
+          return buf if amount==0
+
+          if amount.nil?
+            buf << consume_buffer
+            while chunk = @stream_fiber.resume
+              buf << chunk
+            end
+          else
+            buf << consume_buffer(amount)
+            return buf if buf.length >= amount
+            while chunk = @stream_fiber.resume
+              if buf.length + chunk.length >= amount
+                buf << chunk.slice!(0, amount-buf.length)
+                @buffer = chunk
+                break
+              else
+                buf << chunk
+              end
+            end
+          end
+          return nil if buf.empty? && amount.present?
+          return buf
+        end
+
+        def rewind
+          raise IOError, "stream closed" if @closed
+          @buffer = ''
+          @stream_fiber = Fiber.new do
+            @fedora_file.stream.each do |chunk|
+              Fiber.yield chunk
+            end
+            nil
+          end
+        end
+
+        def close
+          @closed = true
+        end
+
+        private
+
+          def consume_buffer(count=nil)
+            @buffer.slice!(0, count || @buffer.length)
+          end
+      end
+
       def initialize(graph)
         @graph = graph
         @file_cache = {}
@@ -67,7 +124,8 @@ module Hilda
       def get_file(key)
         file = fetch_file(key)
         raise Hilda::Services::FileService::FileNotFoundError, 'File doesn\'t exist' unless file && !file.directory?
-        io = StringIO.new(file.file_contents.content || '')
+        # io = StringIO.new(file.file_contents.content || '')
+        io = FedoraStreamIO.new(file.file_contents)
         if block_given?
           return yield io
         else

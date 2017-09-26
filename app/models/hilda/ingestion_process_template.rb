71792f1525d9b3e1303efa088586bbb630ec0c94
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2017-09-26 15:40:01 +0100

    Add cloning to ingestion process template

--- app/models/hilda/ingestion_process_template.rb
+++ app/models/hilda/ingestion_process_template.rb
@@ -13,22 +13,26 @@ module Hilda
         p.title = "#{self.title || self.template_key || 'Process'} #{DateTime.now.to_formatted_s}"
       end
     end
+    
+    def clone(title, key, description, exists_behaviour=:clean)
+      self.class.new_template(title, key, description, exists_behaviour) do |template|
+        template.from_json( self.module_graph_serialisation.content )
+        template.title = title 
+        template.template_key = key 
+        template.description = description
+        yield template
+      end
+    end
 
     def to_s
       title || template_key || id
     end
 
     def self.new_template(title, key, description, exists_behaviour=:clean)
+      raise 'template key is required' unless key.present?
       existing = Hilda::IngestionProcessTemplate.where(template_key: key)
       raise 'Template already exists' if exists_behaviour==:error && existing.any?
-      return if exists_behaviour==:skip && existing.any?
-
-      if exists_behaviour==:patch && existing.any?
-        old = existing.first
-        yield old
-        old.save
-        return
-      end
+      return existing.first if exists_behaviour==:skip && existing.any?
 
       order_hint = nil
       order_hint = existing.first.order_hint if existing.any?
@@ -43,6 +47,7 @@ module Hilda
       template = Hilda::IngestionProcessTemplate.new(title: title, template_key: key, description: description, order_hint: order_hint)
       yield template
       template.save
+      template
     end
   end
 end
